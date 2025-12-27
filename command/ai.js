bb8c8ba53f3c87771311c823185fa478cbbf0be4
nadell1010 <nadell1010@gmail.com>
2025-12-28 00:04:18 +0700

    Update ai.js

--- command/ai.js
+++ command/ai.js
@@ -1,11 +1,19 @@
+const { chatAI } = require('../lib/ai')
+
 module.exports = {
   name: 'ai',
   execute: async ({ sock, m, args }) => {
     const text = args.join(' ')
-    if (!text) return
+    if (!text) {
+      return sock.sendMessage(m.key.remoteJid, {
+        text: '🗿 ketik pertanyaan bang'
+      })
+    }
+
+    const reply = await chatAI(text)
 
     sock.sendMessage(m.key.remoteJid, {
-      text: `🤖 AI: ${text}`
+      text: `🤖 AI:\n${reply}`
     })
   }
 }
